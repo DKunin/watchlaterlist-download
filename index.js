90dcf0f95b5c3d899f4de6ce8ae68351cf406638
Dmitry Kunin <dkunin@avito.ru>
2016-03-14 15:26:17 +0300

    Styles fixes

--- index.js
+++ index.js
@@ -7,23 +7,23 @@ var app           = express();
 var PORT          = 1999;
 
 app.use(express.static('./public'));
-app.set('view engine', 'jade')
-app.set('views', __dirname + '/views')
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
 
-app.get('/', function(req, res){
-    var files = fs.readdirSync('./public/videos').filter(function(singleFile){
+app.get('/', function(req, res) {
+    var files = fs.readdirSync('./public/videos').filter(function(singleFile) {
         return singleFile.indexOf('mp4') !== -1;
     });
-    res.render('index', { files : files })
+    res.render('index', { files: files });
 });
 
-app.get('/videos/:name', function(req, res){
-    res.render('video', { videoname: req.params.name })
+app.get('/videos/:name', function(req, res) {
+    res.render('video', { videoname: req.params.name });
 });
 
-app.get('/remove/:name', function(req, res){
+app.get('/remove/:name', function(req, res) {
     del(['./public/videos/' + req.params.name]).then(function(paths) {
-         res.send('Deleted files and folders:\n' + paths.join('\n'));
+        res.send('Deleted files and folders:\n' + paths.join('\n'));
     });
 });
 
--- yandex-upload.js
+++ yandex-upload.js
@@ -1,46 +1,44 @@
 'use strict';
 
 var fs = require('fs');
-var path = require('path');
 var YandexDisk = require('yandex-disk').YandexDisk;
 var disk = new YandexDisk(process.env.YADISK_USER, process.env.YADISK_PASS);
 var logUpdate = require('log-update');
-var sanitize = require('sanitize-filename');
-var files = fs.readdirSync('./public/videos').filter(function(singleFile){
+var files = fs.readdirSync('./public/videos').filter(function(singleFile) {
     return singleFile.indexOf('mp4') !== -1;
 });
 
-var promiseExists = function(path, originalName){
+var promiseExists = function(path, originalName) {
     return new Promise(function(resolve, reject) {
-        disk.exists(path, function(err, data){
-            if(err) {
+        disk.exists(path, function(err, data) {
+            if (err) {
                 reject(err);
                 return;
             }
-            resolve({ name: originalName, exists: data});
-        });        
+            resolve({ name: originalName, exists: data });
+        });
     });
-}
+};
 
-var filterOutExisting = function(filesArray){
+var filterOutExisting = function(filesArray) {
     return filesArray.filter(function(singleFilaobject) {
         return singleFilaobject.exists === false;
     });
-}
+};
 
 var singleUpload = function(nonexistentFiles) {
-    disk.uploadFile('./public/videos/' + nonexistentFiles.name, 'videos/' + nonexistentFiles.name, function(err, data){
-        if(err) {
+    disk.uploadFile('./public/videos/' + nonexistentFiles.name, 'videos/' + nonexistentFiles.name, function(err, data) {
+        if (err) {
             logUpdate(err);
             return;
         }
         logUpdate(data);
-    })
-}
+    });
+};
 
 Promise
-    .all(files.map(function(singleFileName){
-        return promiseExists('videos/' + singleFileName,singleFileName);
-    })).then(filterOutExisting).then(function(nonexistentFiles){
+    .all(files.map(function(singleFileName) {
+        return promiseExists('videos/' + singleFileName, singleFileName);
+    })).then(filterOutExisting).then(function(nonexistentFiles) {
         nonexistentFiles.map(singleUpload);
     });
